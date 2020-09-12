fbac1debb4165882dc6d4aa82e58f45759972ba6
Nitin Ranganath <nitinranganath@gmail.com>
2020-09-12 22:23:25 +0530

    Build slider component and initial code

---
+++ src/components/Slider.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="slider-container">
+    <input type="range" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Slider",
+};
+</script>
+
+<style scoped>
+</style>
